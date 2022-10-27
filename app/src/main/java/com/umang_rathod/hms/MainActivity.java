3c1cf756eca542bff73dbde26467d231bf1db075
Umang Rathod <75923134+umang00775@users.noreply.github.com>
2022-10-27 11:30:15 +0530

    Login UI completed

--- app/src/main/java/com/umang_rathod/hms/MainActivity.java
+++ app/src/main/java/com/umang_rathod/hms/MainActivity.java
@@ -2,7 +2,9 @@ package com.umang_rathod.hms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+//                Pair pair = new Pair<View, String>(splashText, "screenTransition");
+            //                    ActivityOptions options =  ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this, pair);
+            startActivity(intent);
+        },1000);
+
     }
 }
